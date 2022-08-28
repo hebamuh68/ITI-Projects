cb44fe56181494b9840b6b17431ccd59c098fa98
Heba Allah. Hashim <69214737+hebamuh68@users.noreply.github.com>
2022-08-28 16:17:10 +0200

    Update HW.js

--- Node.js 12: Getting Started/HW.js
+++ Node.js 12: Getting Started/HW.js
@@ -57,3 +57,62 @@ const greeting = delay =>
     , delay * 1000);
 
 greeting(1);
+
+//=========================================
+/* Challenge 2:
+
+  Just like Challenge 1 but this time with repeated delay values.
+  Print Hello World 5 times with a delay of 100 ms.
+  Then Print it again 5 more times with a delay of 200 ms.
+  Then print it again 5 more times with a delay of 300 ms.
+  And so on until the program is killed.
+
+  Include the delay in the printed message:
+  Hello World. 100
+  Hello World. 100
+  Hello World. 100
+  Hello World. 100
+  Hello World. 100
+  Hello World. 200
+  Hello World. 200
+  Hello World. 200
+  ...
+
+  Constraints:
+   - Only use setInterval (not setTimeout)
+   - Use only ONE if statement */
+
+const greeting = delay =>
+    setTimeout(() => 
+    {
+        for (var i = 0; i < 5; i++) 
+        {
+            console.log('Hello World. ' + delay);
+        }
+        greeting(delay + 100);
+    }, delay );
+
+greeting(100);
+
+//=================================
+
+let lastIntervalId, counter = 5;
+
+const greeting = delay => {
+  if (counter === 5) 
+  {
+    clearInterval(lastIntervalId);
+
+    lastIntervalId = setInterval(() => 
+    {
+      console.log('Hello World. ' + delay);
+      greeting(delay + 100);
+    }, delay);
+    
+    counter = 0;
+  }
+
+  counter += 1;
+};
+
+greeting(100);
